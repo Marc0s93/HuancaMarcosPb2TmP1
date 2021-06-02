259d404db1a20f88c4009385b64fe725726a5062
Marcos Huanca <mhuanca93@gmail.com>
2021-06-02 09:52:22 -0300

    Test de queUnJuegoSeaAlquilable

---
+++ src/ar/edu/unlam/pb2/Alquilable.java
@@ -0,0 +1,8 @@
+package ar.edu.unlam.pb2;
+
+public interface Alquilable {
+	
+	public abstract Double getPrecioAlquiler();
+	public abstract void setPrecioAlquiler(Double precio);
+
+}
--- src/ar/edu/unlam/pb2/Videojuego.java
+++ src/ar/edu/unlam/pb2/Videojuego.java
@@ -1,8 +1,10 @@
 package ar.edu.unlam.pb2;
 
-public class Videojuego extends Producto {
+public class Videojuego extends Producto implements Alquilable {
 	
 	private TipoDeConsola consolaEsperada;
+	
+	private Double precioAlquiler;
 
 	public Videojuego(Integer cODIGO_ESPERADO, String dESCRIPCION_ESPERADA, TipoDeConsola cONSOLA_ESPERADA) {
 		super(cODIGO_ESPERADO,dESCRIPCION_ESPERADA);
@@ -16,6 +18,19 @@ public class Videojuego extends Producto {
 
 	public void setTipo(TipoDeConsola consolaEsperada) {
 		this.consolaEsperada = consolaEsperada;
+	}
+
+
+	@Override
+	public Double getPrecioAlquiler() {
+		
+		return this.precioAlquiler;
+	}
+
+
+	@Override
+	public void setPrecioAlquiler(Double precio) {
+		this.precioAlquiler=precio;
 	}
 
 	
